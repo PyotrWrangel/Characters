30f2b595b16a1e42c2e04b20fb9eab0548ccfac9
PyotrWrangel <maerlyn@hotmail.it>
2025-02-06 17:23:11 +0100

    Updated list component, but needs fixing for item editing

--- src/components/exampleList.jsx
+++ src/components/exampleList.jsx
@@ -1,30 +1,26 @@
+import { useState, useEffect } from "react";
 import { add, toggle, remove } from "../redux/listSlice";
 import { useDispatch, useSelector } from "react-redux";
 
+
 function ExampleList() {
-    const list = useSelector((state) => state.list);
+    const list = useSelector((state) => state.list.value);
     const dispatch = useDispatch();
+    const [inputValue, setInputValue] = useState("");
+
+    useEffect(() => {
+        console.log("List updated:", list);
+    }, [list]);
 
     return (
         <div>
-            <ul>
-                {list.map((item) => (
-                    <li key={item.id}>
-                        <span
-                            onClick={() => dispatch(toggle(item.id))}
-                            style={{
-                                textDecoration: item.completed ? "line-through" : "none",
-                            }}
-                        >
-                            {item.text}
-                        </span>
-                        <button onClick={() => dispatch(remove(item.id))}>Rimuovi</button>
-                    </li>
-                ))}
-            </ul>
-            <button onClick={() => dispatch(add(prompt("Inserisci un nuovo elemento")))}>Aggiungi</button>
+            <p className="mb-3"> TodoList:{list.join(", ")} </p>
+            <input type="text" className="bg-black p-2 rounded-md mb-0" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+            <button className="bg-black p-2 rounded-md mb-0" onClick={() => { dispatch(add(inputValue)); setInputValue("");}}>Aggiungi</button>
+            <button className="bg-black ml-4 mr-4 p-2 rounded-md mb-0" onClick={() => dispatch(toggle())}>Toggle</button>
+            <button className="bg-black p-2 rounded-md mb-0" onClick={() => dispatch(remove())}>Rimuovi</button>
         </div>
-    );
+    )
 }
 
-export default ExampleList;
+export default ExampleList
