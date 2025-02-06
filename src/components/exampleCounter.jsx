343942c5cc9a14695fd6e9f791c25818addf30c8
PyotrWrangel <maerlyn@hotmail.it>
2025-02-06 16:18:32 +0100

    added button fallenRomanEmpire

--- src/components/exampleCounter.jsx
+++ src/components/exampleCounter.jsx
@@ -1,4 +1,4 @@
-import { decrement, increment, reset } from "../redux/counterSlice";
+import { decrement, increment, reset, fallenRomanEmpire } from "../redux/counterSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 function ExampleCounter() {
@@ -8,9 +8,10 @@ function ExampleCounter() {
     return (
         <div>
             <p className="mb-3">Conteggio: {count} </p>
-            <button onClick={() => dispatch(increment())}>Incrementa</button>
-            <button onClick={() => dispatch(decrement())}>Decrementa</button>
-            <button onClick={() => dispatch(reset())}>Reset</button>
+            <button className="bg-black p-2 rounded-md mb-0" onClick={() => dispatch(increment())}>Incrementa</button>
+            <button className="bg-black ml-4 mr-4 p-2 rounded-md mb-0" onClick={() => dispatch(decrement())}>Decrementa</button>
+            <button className="bg-black p-2 rounded-md mb-0" onClick={() => dispatch(reset())}>Reset</button>
+            <button className="bg-black p-2 rounded-md mb-0" onClick={() => dispatch(fallenRomanEmpire())}>Caduta dell Impero Romano</button>
         </div>
     );
 }
