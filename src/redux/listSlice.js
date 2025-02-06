2852a75df7aa9f0e3a106075105ff21f863aa1c5
PyotrWrangel <maerlyn@hotmail.it>
2025-02-06 17:24:04 +0100

    added state.value, must modify toggle

--- src/redux/listSlice.js
+++ src/redux/listSlice.js
@@ -2,9 +2,25 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const listSlice = createSlice({
     name: "list",
-    initialState: [],
+    initialState: {value: []},
+    reducers: {
+        add: (state, action) => {
+            state.value.push(action.payload);
+        },
+        toggle: (state) => {
+            state.value[0] = state.value[0] + " (completato)";
+        },
+        remove: (state) => {
+            state.value.pop();
+        }
+    }
 });
 
 export const { add, toggle, remove } = listSlice.actions;
 
 export const listReducer = listSlice.reducer;
+
+// PROSSIMI PASSI
+// AGGIUNGERE GLI ELEMENTI DI UNA LISTA COME CARD 
+// AGGIUNGERE LO STATO DI COMPLETATO AD UNA ATTIVITA 
+// RIGUARDARE LA RIMOZIONE DELLE ATTIVITA DOPO AVERLA CONTRASSEGNTATA COME COMPLETATA
